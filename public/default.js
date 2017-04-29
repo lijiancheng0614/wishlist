a97a69a1564a82d4cf123220cdf982a57293e13d
lijiancheng0614 <lijiancheng0614@gmail.com>
2017-04-30 03:23:47 +1000

    Fix bugs.

--- public/default.js
+++ public/default.js
@@ -162,16 +162,16 @@ function updateWishList(items, query_facebook_id) {
 		var userPictureUrl = '';
 		Object.keys(items).forEach(function(key) {
 			if (query_facebook_id == '') {
-				if (items[key].facebook_id != null && items[key].facebook_id.includes(facebook_id))
-					continue;
-				userWishListId = 'Friends\'';
-				s += '<li>';
-				// <div style="display: inline-block">
-				s += '<div><img src="' + items[key].img + '"></div>';
-				s += '<div style="margin: 0 20px auto"><h2><a href="' + items[key].item_url + '">' + items[key].product_name + '</a></h2>';
-				s += items[key].price + '<br>';
-				s += '<img src="' + items[key].facebook_picture_url + '"> ' + items[key].facebook_id + '</div>';
-				s += '</li>';
+				if (items[key].facebook_id != null && !items[key].facebook_id.includes(facebook_id)) {
+					userWishListId = 'Friends\'';
+					s += '<li>';
+					// <div style="display: inline-block">
+					s += '<div><img src="' + items[key].img + '"></div>';
+					s += '<div style="margin: 0 20px auto"><h2><a href="' + items[key].item_url + '">' + items[key].product_name + '</a></h2>';
+					s += items[key].price + '<br>';
+					s += '<img src="' + items[key].facebook_picture_url + '"> ' + items[key].facebook_id + '</div>';
+					s += '</li>';
+				}
 			}
 			else if (items[key].facebook_id != null && items[key].facebook_id.includes(query_facebook_id)) {
 				userWishListId = items[key].facebook_id;
