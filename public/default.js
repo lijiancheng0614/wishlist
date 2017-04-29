2fc035bec21a91c7e2fd6ad1911663a8a6119b01
Jeremy Fu <Jeremy Fu>
2017-04-29 16:35:36 +1000

    jeremy

--- public/default.js
+++ public/default.js
@@ -46,9 +46,6 @@ $( document ).ready(function() {
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 
-
-
-
 	// Set up analytics
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
